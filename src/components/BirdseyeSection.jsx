3bdf802c1d5381f37a9c088882ffcd45125eecfb
divyansh730 <divyansh.dabking@gmail.com>
2026-04-05 10:57:27 +0530

    Reduce birdseye gradient to 10px

--- src/components/BirdseyeSection.jsx
+++ src/components/BirdseyeSection.jsx
@@ -113,7 +113,7 @@ export default function BirdseyeSection() {
           bottom: 0,
           left: 0,
           right: 0,
-          height: "30px",
+          height: "10px",
           background: "linear-gradient(to bottom, transparent, #0A0500)",
           pointerEvents: "none",
         }}
